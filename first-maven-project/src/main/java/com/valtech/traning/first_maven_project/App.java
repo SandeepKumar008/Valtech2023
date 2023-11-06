0e3ac393af15cbec63bf46da32e69c5a7ac4f865
SANDEEP KUMAR <92122753+SandeepKumar008@users.noreply.github.com>
2023-11-06 15:47:44 +0530

    Cool

--- first-maven-project/src/main/java/com/valtech/traning/first_maven_project/App.java
+++ first-maven-project/src/main/java/com/valtech/traning/first_maven_project/App.java
@@ -6,8 +6,14 @@ package com.valtech.traning.first_maven_project;
  */
 public class App 
 {
+	public String sayHello() {
+		return "Hello World";
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
 }
+
+
+
